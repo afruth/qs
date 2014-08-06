f6c82000bce5100ef59bddf6c22b5f5b2a35ef1a
Andreas Fruth <andreas.fruth@gmail.com>
2014-08-06 11:34:26 +0300

    ok

--- client/frontpage.js
+++ client/frontpage.js
@@ -18,8 +18,6 @@ Template.mainLayout.events = {
             })
     },
     'click .delSearch': function (e) {
-
-      
         e.preventDefault();
         e.stopPropagation();
         var search = $(e.currentTarget).data('qid');
